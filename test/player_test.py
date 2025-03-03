c70877ce194700ac51924d3ba8f182bdb87a5e12
20115830 <20115830@tafe.wa.edu.au>
2025-03-03 23:46:30 +0800

    feat: add player_node.py and fix unittest code

--- test/player_test.py
+++ test/player_test.py
@@ -8,9 +8,10 @@ class PlayerTest(unittest.TestCase):
         player = Player("1", "HJK")
         self.assertEqual('Player ID: 1, Player Name: HJK', str(player))
 
-    def test_player_id_with_special_chars(self):
-        player = Player("#0123", "HJK")
-        self.assertEqual('Player ID: #0123, Player Name: HJK', str(player))
+    def test_player_uid_and_name_properties(self):
+        player = Player("1", "HJK")
+        self.assertEqual("1", player.uid)
+        self.assertEqual("HJK", player.name)
 
 
 if __name__ == '__main__':
