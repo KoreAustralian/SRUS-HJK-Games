f6775de3d7baf434ad879917194cdbdf177324b8
HONG JAE KIM <20115830@tafe.wa.edu.au>
2025-06-27 00:30:57 +0800

    feat: add recursive insert method

--- app/ player_bst.py
+++ app/ player_bst.py
@@ -1,3 +1,5 @@
+from app.player_bnode import PlayerBNode
+
 class PlayerBST:
     def __init__(self):
         self._root = None
@@ -5,3 +7,29 @@ class PlayerBST:
     @property
     def root(self):
         return self._root
+
+    def insert(self, player):
+        # Insert new Player object into the binary seach tree based on the player's name.
+        if self._root is None:
+            self._root = PlayerBNode(player)
+        else:
+            self.insert_recursive(self._root, player)
+
+    def insert_recursive(self, current_node, player):
+        key = player.name
+        current_key = current_node.player.name
+
+        if key == current_key:
+            current_node.player = player
+        elif key < current_key:
+            # Insert into the left subtree
+            if current_node.left is None:
+                current_node.left = PlayerBNode(player)
+            else:
+                self.insert_recursive(current_node.left, player)
+        else:
+            # Insert into the right subtree
+            if current_node.right is None:
+                current_node.right = PlayerBNode(player)
+            else:
+                self.insert_recursive(current_node.right, player)
